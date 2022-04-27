8723c8687190abd182b6f4b729729816532452c2
Keunjae Jeong <38673566+TPTplayer@users.noreply.github.com>
2022-04-27 17:24:11 +0900

    Delete projects/file_monitoring directory

--- projects/file_monitoring/util.h
+++
@@ -1,8 +0,0 @@
-#define PATH_LEN 512
-#define TIME_LEN 20
-
-void error_handler(int err, char *func);
-int stdio_err_redirection(char *file_log, char *file_err);
-int get_current_time(char *buf);
-int set_config(void);
-int read_config(char *path, char *file_log, char *file_err);
